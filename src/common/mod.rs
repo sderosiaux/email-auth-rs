3f588d66086aa0473f0b384c22517eb59f7e40d9
Stephane Derosiaux <sderosiaux@conduktor.io>
2026-02-05 03:32:12 -0500

    Remove implementation, keep specs with learnings
    Reset for fresh implementation with improved process:
    - Sub-Specs table added to IMPL
    - All milestone checkboxes reset
    - Learnings preserved from previous attempt

--- src/common/mod.rs
+++
@@ -1,7 +0,0 @@
-mod dns;
-mod domain;
-mod psl;
-
-pub use dns::{DnsError, DnsResolver, HickoryResolver, MockResolver};
-pub use domain::{normalize_domain, parse_email_domain, parse_email_local};
-pub use psl::organizational_domain;
