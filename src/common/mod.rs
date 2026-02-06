fa347c978f619e3780b4b54c1d739bd2e16b7945
Stephane Derosiaux <sderosiaux@conduktor.io>
2026-02-06 04:24:35 +0100

    Remove implementation, keep specs with learnings
    Fresh start for reimplementation using enriched IMPL.md spec.

--- src/common/mod.rs
+++
@@ -1,7 +0,0 @@
-mod dns;
-mod domain;
-mod psl;
-
-pub use dns::{DnsResolver, DnsError, HickoryResolver, MockResolver};
-pub use domain::{normalize_domain, strip_trailing_dot};
-pub use psl::organizational_domain;
