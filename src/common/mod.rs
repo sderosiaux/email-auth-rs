825d92f87798e0c024b1cf483559abc9afb2f863
Stephane Derosiaux <sderosiaux@conduktor.io>
2026-02-07 08:56:02 +0100

    Remove implementation, keep specs only

--- src/common/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod dns;
-pub mod domain;
-pub mod psl;
--- src/dmarc/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod eval;
-pub mod record;
-
-pub use eval::{Disposition, DmarcEvaluator, DmarcResult};
-pub use record::{
-    AlignmentMode, DmarcError, DmarcRecord, FailureOption, Policy, ReportFormat, ReportUri,
-};
